39dcfd711673cc66fddcf1d4edde01709ef13fc8
wolf <doggflixx@gmail.com>
2025-09-14 19:09:39 -0500

    fix(vercel): route /s/* -> Flask + catch-all

--- api/index.py
+++ api/index.py
@@ -1,9 +1,1 @@
-from app import create_app
-
-app = create_app()
-
-try:
-    from werkzeug.middleware.proxy_fix import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
-except Exception:
-    pass
+from app import app as app
